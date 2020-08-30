567b8c3b6e2849efe59787c77d2904f7b5e8bbda
pengliwei <562150160@qq.com>
2020-08-30 10:26:08 +0800

    初次代码提交

---
+++ server/src/main/java/learn/A.java
@@ -0,0 +1,13 @@
+package learn;
+
+/**
+ * @description: Test
+ * @author: PENGLW
+ * @date: 2020/8/30
+ */
+public class A {
+
+    public static void main(String[] args) {
+        System.out.println("11");
+    }
+}
